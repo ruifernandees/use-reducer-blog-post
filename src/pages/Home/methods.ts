6ca179258e139212889b421f495d772b4f3f3b8c
ruifernandees <dev.ruifernandes@gmail.com>
2022-07-30 17:37:05 -0300

    refactor: use object literals on reducer

--- src/pages/Home/methods.ts
+++ src/pages/Home/methods.ts
@@ -1,36 +1,37 @@
+import colors from 'tailwindcss/colors';
 import { AlertIcon } from '../../assets/icons/AlertIcon';
 import { CheckIcon } from '../../assets/icons/CheckIcon';
 import { XIcon } from '../../assets/icons/XIcon';
-import colors from 'tailwindcss/colors';
 import { ActionProps, StateProps } from "./props";
 
-export function reducer(state: StateProps, action: ActionProps): StateProps {
-  switch (action.mode) {
-    case 'error':
-      return {
-        icon: XIcon,
-        color: colors.red[500],
-        message: 'Unexpected error!'
-      }
-    case 'success':
-      return {
-        icon: CheckIcon,
-        color: colors.green[500],
-        message: 'Success!'
-      }
-    case 'warning':
-      return {
-        icon: AlertIcon,
-        color: colors.yellow[500],
-        message: 'This is a warning'
-      }
-    case 'hidden':
-      return {
-        icon: CheckIcon,
-        color: '',
-        message: ''
-      }
-    default:
-      throw new Error();
+type modesType = {[key: string]: StateProps};
+
+const modes: modesType = {
+  error: {
+    icon: XIcon,
+    color: colors.red[500],
+    message: 'Unexpected error!'
+  },
+  success: {
+    icon: CheckIcon,
+    color: colors.green[500],
+    message: 'Success!'
+  },
+  warning: {
+    icon: AlertIcon,
+    color: colors.yellow[500],
+    message: 'This is a warning'
+  },
+  hidden: {
+    icon: CheckIcon,
+    color: '',
+    message: ''
   }
+}
+
+export function reducer(state: StateProps, action: ActionProps): StateProps {
+  if (!(action.mode in modes)) {
+    throw new Error();
+  }
+  return modes[action.mode];
 }
